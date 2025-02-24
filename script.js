e8bb3ee4da23c3985a806ad91e44dd2a438184b5
Undominel <info@relocat.agency>
2025-02-24 20:13:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-// Инициализация Telegram Web App
+// Подключение Telegram WebApp API
 let tg = window.Telegram.WebApp;
-tg.expand(); // Растягиваем на весь экран
+tg.expand(); // Делаем мини-апп на весь экран
 
-console.log("Telegram Mini App gestartet!");
+// Проверяем, загружен ли Telegram Web App
+tg.ready();
+console.log("GermanBuddy Mini App gestartet!");
